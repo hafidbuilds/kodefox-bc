9b2a1e41efa6268795aeb4c84fd75b59cf4853d2
hafidz <hafidzzpermana09@gmail.com>
2018-04-26 06:24:26 +0700

    Refine

--- Day02/src/__tests__/compareCombine.test.js
+++ Day02/src/__tests__/compareCombine.test.js
@@ -1,2 +1,41 @@
-/* eslint-disable */
-import {compareCombine} from '../compareCombine.js';
+import compareCombine from '../compareCombine.js';
+
+it('should consider empty objects to be equal', () => {
+  let result = compareCombine({}, {});
+  expect(result).toEqual(true);
+});
+
+it('should consider objects to be equal', () => {
+  let result = compareCombine({one: 1, two: 2}, {two: 2, one: 1});
+  expect(result).toEqual(true);
+});
+
+it('should consider undefined object to be unequal', () => {
+  let result = compareCombine({one: 1, two: undefined}, {two: 2, one: 1});
+  expect(result).toEqual(false);
+});
+
+it('should consider empty arrays to be equal', () => {
+  let result = compareCombine({city: ['jkt', 'tng']}, {city: ['tng', 'jkt']});
+  expect(result).toEqual(false);
+});
+
+it('should consider empty arrays to be equal', () => {
+  let result = compareCombine([], []);
+  expect(result).toEqual(true);
+});
+
+it('should consider empty arrays to be equal', () => {
+  let result = compareCombine([{name: 'Hafidz'}, {nationality: 'INA'}], [{nationality: 'INA'}, {name: 'Hafidz'}]);
+  expect(result).toEqual(false);
+});
+
+it('should consider empty arrays to be equal', () => {
+  let result = compareCombine('string', 'string');
+  expect(result).toEqual(false);
+});
+
+it('should consider empty arrays to be equal', () => {
+  let result = compareCombine(200, true);
+  expect(result).toEqual(false);
+});
--- Day02/src/compareCombine.js
+++ Day02/src/compareCombine.js
@@ -1,35 +1,30 @@
 // @flow
 /* eslint-disable */
 
-function combineCompare(thing1: any, thing2: any) {
+function compareCombine(thing1: mixed, thing2: mixed) {
   if (thing1 == null || thing2 == null) {
     return false
   }
   // checking all primitives cases
   if (
-    typeof thing1 === 'number' || typeof thing1 === 'string' || typeof thing1 === 'boolean'
-    &&
+    typeof thing1 === 'number' || typeof thing1 === 'string' || typeof thing1 === 'boolean' &&
     typeof thing2 === 'number' || typeof thing2 === 'string' || typeof thing2 === 'boolean'
   ) {
     return false
   }
   // checking non-primitives cases (Array vs Array)
-  if (isAnArrays(thing1, thing2)) {
+  if (Array.isArray(thing1) && Array.isArray(thing2)) {
     return compareArrays(thing1, thing2)
   }
-  // checking Objects cases
-  if (isAnObjects(thing1, thing2)) {
+  // checking Objects cases (Array vs Obj)
+  if (
+    typeof thing1 === 'object' && !Array.isArray(thing1) && thing1 != null 
+    && 
+    typeof thing2 === 'object' && !Array.isArray(thing2) && thing2 != null
+  ) {
     return compareObjects(thing1, thing2)
   }
-  return false
-}
-
-function isAnArrays(arr1: Array<mixed>, arr2: Array<mixed>) {
-  if (Array.isArray(arr1) && Array.isArray(arr2)) {
-    return true
-  } else {
-    return false
-  }
+  return true
 }
 
 function compareArrays(arr1: Array<mixed>, arr2: Array<mixed>) {
@@ -44,35 +39,45 @@ function compareArrays(arr1: Array<mixed>, arr2: Array<mixed>) {
   return true
 }
 
-function isAnObjects(obj1: Object, obj2: Object) {
-  // check if obj1 && obj2 is an Object
-  if (
-    typeof obj1 === 'object' && !Array.isArray(obj1) && obj1 != null
-    &&
-    typeof obj2 === 'object' && !Array.isArray(obj2) && obj2 != null
-  ) {
-    return true
-  }
-}
-
 function compareObjects(obj1: Object, obj2: Object) {
   let keys1 = Object.keys(obj1)
   let keys2 = Object.keys(obj2)
 
   if (keys1.length !== keys2.length) {
     return false
-  }
+  } 
   for (let key of keys1) {
-    if (keys1[key] !== keys2[key]) {
+    if (obj1[key] !== obj2[key]) {
       return false
     }
   }
   for (let key of keys2) {
-    if (keys2[key] !== keys2[key]) {
+    if (obj1[key] !== obj2[key]) {
       return false
     }
   }
   return true
 }
 
-export {compareArrays, compareObjects, combineCompare, isAnArrays, isAnObjects}
+// function isAnObjects(obj1: Object, obj2: Object) {
+//   // check if obj1 && obj2 is an Object
+//   if (
+//     typeof obj1 === 'object' && !Array.isArray(obj1) && obj1 != null 
+//     && 
+//     typeof obj2 === 'object' && !Array.isArray(obj2) && obj2 != null
+//   ) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// function isAnArrays(arr1: Array<mixed>, arr2: Array<mixed>) {
+//   if (Array.isArray(arr1) && Array.isArray(arr2)) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+export default compareCombine
