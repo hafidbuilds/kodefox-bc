02080133a003c101fd58a8d298f8c53121a14a2e
hafidz <hafidzzpermana09@gmail.com>
2018-04-27 00:12:36 +0700

    Make flow 100% happy

--- Day02/src/compareCombine.js
+++ Day02/src/compareCombine.js
@@ -39,19 +39,23 @@ function compareArrays(arr1: Array<mixed>, arr2: Array<mixed>): boolean {
   return true
 }
 
-function compareObjects(obj1: {}, obj2: {}): boolean {
+type AnObject = {
+  [key: string]: mixed
+}
+
+function compareObjects(obj1: AnObject, obj2: AnObject): boolean {
   let keys1: Array<string> = Object.keys(obj1)
   let keys2: Array<string> = Object.keys(obj2)
 
   if (keys1.length !== keys2.length) {
     return false
   } 
-  for (let key: string of keys1) {
+  for (let key of keys1) {
     if (obj1[key] !== obj2[key]) {
       return false
     }
   }
-  for (let key: string of keys2) {
+  for (let key of keys2) {
     if (obj1[key] !== obj2[key]) {
       return false
     }
@@ -60,9 +64,9 @@ function compareObjects(obj1: {}, obj2: {}): boolean {
 }
 
 
-// Another approach
+// Another approach but there is flow error msg i dont understand yet
 
-// function isAnObjects(obj1: {}, obj2: {}): boolean {
+// function isAnObjects(obj1: AnObject, obj2: AnObject): boolean {
 //   // check if obj1 && obj2 is an Object
 //   if (
 //     typeof obj1 === 'object' && !Array.isArray(obj1) && obj1 != null 
--- Day02/src/compareObjects.js
+++ Day02/src/compareObjects.js
@@ -2,7 +2,11 @@
 /* eslint-disable */
 // @flow
 
-function compareObjects(obj1: {}, obj2: {}): boolean {
+type AnObject = {
+  [key: string]: mixed
+}
+
+function compareObjects(obj1: AnObject, obj2: AnObject): boolean {
   let prop1: Array<string> = Object.keys(obj1)
   let prop2: Array<string> = Object.keys(obj2)
 
@@ -10,12 +14,12 @@ function compareObjects(obj1: {}, obj2: {}): boolean {
     return false
   }
   
-  for (let key: string of prop1) {
+  for (let key of prop1) {
     if (obj1[key] !== obj2[key]) {
       return false
     }
   }
-  for (let key: string of prop2) {
+  for (let key of prop2) {
     if (obj1[key] !== obj2[key]) {
       return false
     }
