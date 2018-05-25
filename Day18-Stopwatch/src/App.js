940c4ae8ddae994637a3ca1e3594d42fa1e9e624
Hafidz <Me@coding-station.lan>
2018-05-25 18:40:34 +0700

    init

--- Day18-Stopwatch/src/App.js
+++ Day18-Stopwatch/src/App.js
@@ -1,33 +1,33 @@
-// import React from 'react';
-// import {StyleSheet, Text, View} from 'react-native';
-// import {ButtonRow, LapsTable, RoundButton, Timer} from './components';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {ButtonRow, LapsTable, RoundButton, Timer} from './components';
 
-// const DATA = {
-//   timer: 1234567,
-//   laps: [12345, 09876, 65432],
-// };
+const DATA = {
+  timer: 1234567,
+  laps: [12345, 49876, 65432],
+};
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Timer interval={DATA.timer} />
-//         <ButtonRow>
-//           <RoundButton title="Reset" color="#FFFFFF" background="#3D3D3D" />
-//           <RoundButton title="Start" color="#8B8B90" background="#1B361F" />
-//         </ButtonRow>
-//         <LapsTable laps={DATA.laps} />
-//       </View>
-//     );
-//   }
-// }
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Timer interval={DATA.timer} />
+        <ButtonRow>
+          <RoundButton title="Reset" color="#FFFFFF" background="#3D3D3D" />
+          <RoundButton title="Start" color="#8B8B90" background="#1B361F" />
+        </ButtonRow>
+        <LapsTable laps={DATA.laps} />
+      </View>
+    );
+  }
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#0D0D0D',
-//     alignItems: 'center',
-//     paddingTop: 130,
-//     paddingHorizontal: 20,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0D0D0D',
+    alignItems: 'center',
+    paddingTop: 130,
+    paddingHorizontal: 20,
+  },
+});
